0b5a7092fe7f57aba194e847c0cef5c02bebfe18
Marco Roth <marco.roth@intergga.ch>
2023-10-20 02:50:24 +0200

    Use `Page::CardSectionComponent` for index pages

--- app/components/page/card_section_component.rb
+++ app/components/page/card_section_component.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class Page::CardSectionComponent < ViewComponent::Base
-  def initialize(title:, resources:, limit: 3, all_url: nil)
+  def initialize(resources:, title: nil, all_url: nil)
     @title = title
     @resources = resources
-    @limit = limit
     @all_url = all_url
   end
 end
