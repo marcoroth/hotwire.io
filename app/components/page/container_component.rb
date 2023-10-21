dc9aa57d17e7644a65005805446b1de08727d170
Martin Arce <54085409+divagueame@users.noreply.github.com>
2023-10-21 21:27:15 +0200

    Breadcrumbs for page navigation (#46)
    * Instructions to start the project locally
    * Add breadcrumbs
    * Move breadcrumbs to Container component

--- app/components/page/container_component.rb
+++ app/components/page/container_component.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 class Page::ContainerComponent < ViewComponent::Base
+  renders_one :breadcrumbs, "UI::BreadcrumbsComponent"
   renders_one :title, "UI::TitleComponent"
   renders_one :authors, "Page::AuthorsComponent"
 
-  def initialize(page:)
+  def initialize(page:, options: {})
     @page = page
+    @options = options
+  end
+
+  def render_breadcrumbs?
+    @options.fetch(:render_breadcrumbs, true)
   end
 
   def credit_paths
