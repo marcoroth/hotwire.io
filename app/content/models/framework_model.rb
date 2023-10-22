b99eaf1ad74cfb7602027a4501050e687b697beb
Marco Roth <marco.roth@intergga.ch>
2023-10-22 07:48:31 +0200

    Group frameworks on frameworks page by language

--- app/content/models/framework_model.rb
+++ app/content/models/framework_model.rb
@@ -1,4 +1,8 @@
 class FrameworkModel < Sitepress::Model
-  collection glob: "**/frameworks/*.html*"
+  collection glob: "**/frameworks/*.html*", sort: :language
   data :title, :framework, :language, :description, :icon_class, :image
+
+  def self.grouped_by_language
+    all.sort_by(&:title).group_by(&:language).sort_by { |language, _| (language == "Ruby") ? "" : language }
+  end
 end
