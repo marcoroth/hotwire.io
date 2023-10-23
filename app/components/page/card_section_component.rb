46c4eaa71cefda8e9c8d1fd3f10853468db6d765
Marco Roth <marco.roth@intergga.ch>
2023-10-23 05:10:21 +0200

    Show 6 cards per ecosystem section and show how many more are remaining

--- app/components/page/card_section_component.rb
+++ app/components/page/card_section_component.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class Page::CardSectionComponent < ViewComponent::Base
-  def initialize(resources:, title: nil, all_url: nil)
+  def initialize(resources:, title: nil, all_url: nil, count: nil)
     @title = title
-    @resources = resources
+    @count = count
     @all_url = all_url
+
+    resources = Array.wrap(resources)
+
+    @resources = @count ? resources.take(@count) : resources
+    @total = resources.count
   end
 end
