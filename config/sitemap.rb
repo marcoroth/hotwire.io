576e6d89de67af9f81787f977e88c5a22cbdd7a3
Joan Nyambura <njugunajoan35@gmail.com>
2023-10-24 13:23:09 +0300

    Update URLs to use non-www domain and change sitemap format
    - Change https://www.hotwire.io to https://hotwire.io
    - Change https://www.hotwire.io/sitemap.xml to https://hotwire.io/sitemap.xml.gz

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://www.hotwire.io"
+SitemapGenerator::Sitemap.default_host = "https://hotwire.io"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
