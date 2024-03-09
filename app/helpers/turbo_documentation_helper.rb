b606527616d2562ffa1feddd20948129922e5fdc
Marco Roth <marco.roth@intergga.ch>
2024-03-09 01:22:01 +0100

    Fix Turbo documentation

--- app/helpers/turbo_documentation_helper.rb
+++ app/helpers/turbo_documentation_helper.rb
@@ -2,6 +2,10 @@
 
 module TurboDocumentationHelper
   def turbo_documentation_page(page)
-    render_markdown(Net::HTTP.get(URI("https://raw.githubusercontent.com/hotwired/turbo-site/main/_source/#{page}")).split("---").last.force_encoding("utf-8"))
+    ref = "692c6ffe873d4671d6c749eb045c073bae0e032e"
+    content = Net::HTTP.get(URI("https://raw.githubusercontent.com/hotwired/turbo-site/#{ref}/_source/#{page}"))
+    content_without_frontmatter = content.split("---").from(2).join("---")
+
+    render_markdown(content_without_frontmatter.force_encoding("utf-8"))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
+  scope "/documentation/turbo/reference" do
+    get "/01_drive", to: redirect("/documentation/turbo/handbook/drive")
+    get "/02_frames", to: redirect("/documentation/turbo/handbook/frames")
+    get "/03_streams", to: redirect("/documentation/turbo/handbook/streams")
+    get "/04_events", to: redirect("/documentation/turbo/handbook/events")
+    get "/05_attributes", to: redirect("/documentation/turbo/handbook/attributes")
+  end
+
+  scope "/documentation/turbo/handbook" do
+    get "/03_frames", to: redirect("/documentation/turbo/handbook/frames")
+    get "/04_frames", to: redirect("/documentation/turbo/handbook/frames")
+
+    get "/04_streams", to: redirect("/documentation/turbo/handbook/streams")
+    get "/05_streams", to: redirect("/documentation/turbo/handbook/streams")
+
+    get "/05_native", to: redirect("/documentation/turbo/handbook/native")
+    get "/06_native", to: redirect("/documentation/turbo/handbook/native")
+
+    get "/06_building", to: redirect("/documentation/turbo/handbook/building")
+    get "/07_building", to: redirect("/documentation/turbo/handbook/building")
+
+    get "/07_installing", to: redirect("/documentation/turbo/handbook/installing")
+    get "/08_installing", to: redirect("/documentation/turbo/handbook/installing")
+  end
+
   sitepress_pages
   sitepress_root
 
