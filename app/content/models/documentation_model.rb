80d30624ddac8cc3e6461762a594a8bf9b0866a0
Ash Joseph <kamikazedesigns@gmail.com>
2025-01-27 02:22:49 +0000

    Documentation page improvements (#420)
    * Added status frontmatter to documentation pages
    
    * Added status to resource card component
    
    For displaing the state of a page, saving end users a click and frustration/assumption the site is empty
    
    * Added status to frameworks page
    
    * Displaying core techologies at the top of the documentation page
    
    * Added missing status to frameworks without page content
    
    * Update resource_card_component.html.erb

--- app/content/models/documentation_model.rb
+++ app/content/models/documentation_model.rb
@@ -1,4 +1,12 @@
 class DocumentationModel < Sitepress::Model
   collection glob: "**/documentation/*.html*"
   data :title
+
+  def self.core_technologies
+    all.filter { |page| page.data["core"] == true }.sort_by(&:title)
+  end
+
+  def self.third_party_technologies
+    all.filter { |page| page.data["core"] != true }.sort_by(&:title)
+  end
 end
--- app/content/models/framework_model.rb
+++ app/content/models/framework_model.rb
@@ -1,6 +1,6 @@
 class FrameworkModel < Sitepress::Model
   collection glob: "**/frameworks/*.html*"
-  data :title, :framework, :language, :description, :icon_class, :image
+  data :title, :framework, :language, :description, :icon_class, :image, :status
 
   def self.grouped_by_language
     all.sort_by(&:title).group_by(&:language).sort_by { |language, _| (language == "Ruby") ? "" : language }
